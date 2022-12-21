559297f1fe3fab5cd33e2df90e0d4251fe2beb4c
Sarthak S Kumar <sskworld9742@gmail.com>
2022-12-21 21:19:32 +0530

    add: change icon display for theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,7 +205,7 @@ if (selectedTheme) {
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
-  themeButton.classList[selectedIcon === "uil-sun" ? "add" : "remove"](
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
     iconTheme
   );
 }
